0b74209e461217d42a7af7e6df27eeb4a33084ec
Randy Rivera <rivera.na2015@gmail.com>
2022-09-26 15:14:27 -0400

    area of rectangle formula

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,10 @@
+// to find the area of a rectangle we multiply the length of the rectangle byu the width of the rectangle.
+
+var lenOfRectangle = 23;
+var widOfRectangle = 10;
+
+// solution
+
+areaOfRec = widOfRectangle * lenOfRectangle;
+
+console.log("The area of the rectangle is " + areaOfRec + ".");
