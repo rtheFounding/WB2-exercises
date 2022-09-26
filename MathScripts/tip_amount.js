f821ae1c7fda4fcb8003d3e6ba4c46e43f2211f3
Randy Rivera <rivera.na2015@gmail.com>
2022-09-26 15:26:05 -0400

    Exercise 2-3 tip

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,6 @@
+var tipPercentage = 20;
+var totalCost = 48.00;
+
+var tipTotal = totalCost * tipPercentage/100;
+
+console.log("The tip on a $" + totalCost.toFixed(2) + " is " + tipTotal.toFixed(2) + ".");
