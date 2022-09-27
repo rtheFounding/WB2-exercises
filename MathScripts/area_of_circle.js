d84cf51fe416a73c8800aa810d028f43506bb519
Randy Rivera <rivera.na2015@gmail.com>
2022-09-26 23:51:04 -0400

    Area of circle formula

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,6 @@
+
+var numRadius = 20;
+
+var areaOfCircle = Math.PI * Math.pow(20, 2);
+console.log(areaOfCircle);
+console.log("The area of a circle with a radius " + numRadius + " is " + areaOfCircle + ".");
