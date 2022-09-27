a38cb068006fcfbda84a7783812630dd50b2ad5c
Randy Rivera <rivera.na2015@gmail.com>
2022-09-27 13:34:00 -0400

    Section 3-20 Exercise

---
+++ SwitchScripts/dept_converter.js
@@ -0,0 +1,26 @@
+let deparmentCode = 10;
+let deparmentName;
+
+switch(deparmentCode) {
+    case 1:
+        console.log("Marketing");
+        break;
+    case 5:
+        console.log("Human Resources");
+        break;
+    case 10:
+        console.log("Acounting");
+        break;
+    case 12:
+        console.log("Legal");
+        break;
+    case 18:
+        console.log("IT");
+        break;
+    case 20:
+        console.log("Customer Relations");
+        break;
+    default:
+        console.log("<unknown>");
+        
+}
