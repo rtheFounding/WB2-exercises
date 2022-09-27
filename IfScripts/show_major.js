b2239ac0c69ebddd8e588492bd7edef67c212ed0
Randy Rivera <rivera.na2015@gmail.com>
2022-09-27 13:14:38 -0400

    given student/Major code, finding location

---
+++ IfScripts/show_major.js
@@ -0,0 +1,24 @@
+var studentName = "Gabe";
+var studentMajor = "CSCI";
+
+if (studentMajor == "CSCI") {
+    console.log("Student: " + studentName + "");
+    console.log("Major: Computer Science" );
+    console.log("Advising Location: Sheppard Hall, Room 314");
+} else if (studentMajor == "BIOL") {
+    console.log("Student: " + studentName + "");
+    console.log("Major: Biology" );
+    console.log("Advising Location: Science Bldg, Room 314");
+} else if (studentMajor == "ENG") {
+    console.log("Student: " + studentName + "");
+    console.log("Major: English " );
+    console.log("Advising Location: Kerr Hall, Room 201");
+} else if (studentMajor == "HIST") {
+    console.log("Student: " + studentName + "");
+    console.log("Major: History" );
+    console.log("Advising Location: Kerr Hall, Room 114");
+} else if (studentMajor == "MKT") {
+        console.log("Student: " + studentName + "");
+        console.log("Major: Marketing" );
+        console.log("Advising Location: Westly Hall, Room 310");
+}
