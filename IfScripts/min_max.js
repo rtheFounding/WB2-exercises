5b56b6c6ba2c370d507319c1d9d3620ff92f6233
Randy Rivera <rivera.na2015@gmail.com>
2022-09-27 11:26:14 -0400

    without Math.min/Math.max

---
+++ IfScripts/min_max.js
@@ -0,0 +1,18 @@
+a = 123;
+b = 34;
+c = 511;
+
+if (a < b && a < c) {
+    console.log(a)
+} else if (b < a && b < c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
+if (a > b && a > c) {
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
