a77e645adfcfba9a6ba01172dfdf1443b0361c30
Randy Rivera <rivera.na2015@gmail.com>
2022-09-27 09:53:02 -0400

    simple script using if/ else if/ else statements.

---
+++ IfScripts/greeting.js
@@ -0,0 +1,11 @@
+var currentHour = 17;
+
+if (currentHour <= 9) {
+ console.log("Good morning!");
+}
+else if (currentHour <= 16.59) {
+ console.log("Good Day!");
+}
+else {
+    console.log("Good Evening!");
+}
