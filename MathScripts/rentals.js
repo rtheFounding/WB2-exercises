6eef3645fad28c75d197d311e7b32df501106df2
Randy Rivera <rivera.na2015@gmail.com>
2022-09-27 02:01:08 -0400

    Section 2-20 Rentals hand calculations

---
+++ MathScripts/rentals.js
@@ -0,0 +1,13 @@
+var numOfPeople = 38;
+
+var costOfVans = 250 * 3;
+
+var costSplit = costOfVans / 38;
+
+console.log(numOfPeople, costOfVans.toFixed(2), Math.round(costSplit));
+
+
+// I charged 20 dollars per person
+// When I multiplied that out I collected a total of 760 dollars.
+// The vans for 38 people was 750
+// I had 10 dollars left over.
